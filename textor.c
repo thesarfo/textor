c1f4a958491ba5604bb8ee014e205d2eafa895cb
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-04-02 22:17:15 +0100

    editorSetStatusMessage with variadic arguments

--- textor.c
+++ textor.c
@@ -1,106 +1,107 @@
-/*** includes ***/
+#define TEXTOR_VERSION "0.0.1"
 
-#include <ctype.h>
-#include <errno.h>
-#include <stdio.h>
-#include <stdlib.h>
+#ifdef __linux__
+#define _POSIX_C_SOURCE 200809L
+#endif
+
 #include <termios.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <errno.h>
+#include <string.h>
+#include <ctype.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/ioctl.h>
+#include <sys/time.h>
 #include <unistd.h>
+#include <stdarg.h>
+#include <fcntl.h>
+#include <signal.h>
 
+/* Syntax highlight types */
+#define HL_NORMAL 0
+#define HL_NONPRINT 1
+#define HL_COMMENT 2   /* Single line comment. */
+#define HL_MLCOMMENT 3 /* Multi-line comment. */
+#define HL_KEYWORD1 4
+#define HL_KEYWORD2 5
+#define HL_STRING 6
+#define HL_NUMBER 7
+#define HL_MATCH 8      /* Search match. */
 
-/*** defines ***/
-#define CTRL_KEY(k) ((k) & 0x1f)
+#define HL_HIGHLIGHT_STRINGS (1<<0)
+#define HL_HIGHLIGHT_NUMBERS (1<<1)
 
-/*** data ***/
+struct editorSyntax {
+    char **filematch;
+    char **keywords;
+    char singleline_comment_start[2];
+    char multiline_comment_start[3];
+    char multiline_comment_end[3];
+    int flags;
+};
 
-struct termios orig_termios;
+/* This structure represents a single line of the file we are editing. */
+typedef struct erow {
+    int idx;            /* Row index in the file, zero-based. */
+    int size;           /* Size of the row, excluding the null term. */
+    int rsize;          /* Size of the rendered row. */
+    char *chars;        /* Row content. */
+    char *render;       /* Row content "rendered" for screen (for TABs). */
+    unsigned char *hl;  /* Syntax highlight type for each character in render.*/
+    int hl_oc;          /* Row had open comment at end in last syntax highlight
+                           check. */
+} erow;
 
-/*** terminal ***/
+typedef struct hlcolor {
+    int r,g,b;
+} hlcolor;
 
-void die(const char *s){
-	write(STDOUT_FILENO, "\x1b[2J", 4);
-  	write(STDOUT_FILENO, "\x1b[H", 3);
+struct editorConfig {
+    int cx,cy;  /* Cursor x and y position in characters */
+    int rowoff;     /* Offset of row displayed. */
+    int coloff;     /* Offset of column displayed. */
+    int screenrows; /* Number of rows that we can show */
+    int screencols; /* Number of cols that we can show */
+    int numrows;    /* Number of rows */
+    int rawmode;    /* Is terminal raw mode enabled? */
+    erow *row;      /* Rows */
+    int dirty;      /* File modified but not saved. */
+    char *filename; /* Currently open filename */
+    char statusmsg[80];
+    time_t statusmsg_time;
+    struct editorSyntax *syntax;    /* Current syntax highlight, or NULL. */
+};
 
-	perror(s);
-	exit(1);
-}
+static struct editorConfig E;
 
-void disableRawMode(){
-	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1);
-		die("tcsetattr");
-}
+enum KEY_ACTION{
+        KEY_NULL = 0,       /* NULL */
+        CTRL_C = 3,         /* Ctrl-c */
+        CTRL_D = 4,         /* Ctrl-d */
+        CTRL_F = 6,         /* Ctrl-f */
+        CTRL_H = 8,         /* Ctrl-h */
+        TAB = 9,            /* Tab */
+        CTRL_L = 12,        /* Ctrl+l */
+        ENTER = 13,         /* Enter */
+        CTRL_Q = 17,        /* Ctrl-q */
+        CTRL_S = 19,        /* Ctrl-s */
+        CTRL_U = 21,        /* Ctrl-u */
+        ESC = 27,           /* Escape */
+        BACKSPACE =  127,   /* Backspace */
+        /* The following are just soft codes, not really reported by the
+         * terminal directly. */
+        ARROW_LEFT = 1000,
+        ARROW_RIGHT,
+        ARROW_UP,
+        ARROW_DOWN,
+        DEL_KEY,
+        HOME_KEY,
+        END_KEY,
+        PAGE_UP,
+        PAGE_DOWN
+};
 
-
-void enableRawMode() {
- 	if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
- 	atexit(disableRawMode);
-
-	struct termios raw = orig_termios;
-	raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
-	raw.c_oflag &= ~(OPOST);
-	raw.c_cflag != (CS8);
-	raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
-	raw.c_cc[VMIN] = 0;	
-	raw.c_cc[VTIME] = 1;
-
-	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
-
-  	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
-}
-
-
-char editorReadKey(){
-	int nread;
-	char c;
-	while ((nread = read(STDIN_FILENO, &c, 1)) != 1){
-		if (nread == -1 && errno != EAGAIN) die("read");
-	}
-	return c;
-}
-
-
-/*** output ***/
-void editorDrawRows() {
-  int y;
-  for (y = 0; y < 24; y++) {
-    write(STDOUT_FILENO, "~\r\n", 3);
-  }
-}
-
-
-void editorRefreshScreen(){
-	write(STDOUT_FILENO, "\x1b[2J", 4);
-	write(STDOUT_FILENO, "\x1b[H", 3);
-
-	editorDrawRows();
-	
-  	write(STDOUT_FILENO, "\x1b[H", 3);
-}
-
-/*** input ***/
-void editorProcessKeypress(){
-	char c = editorReadKey();
-
-	switch (c)
-	{
-	case CTRL_KEY('q'):
-		write(STDOUT_FILENO, "\x1b[2J", 4);
-        write(STDOUT_FILENO, "\x1b[H", 3);
-		exit(0);
-		break;
-	}
-}
-
-
-/*** init ***/
-
-int main() {
- 	enableRawMode();
-
-
- 	while (1) {		
-		editorRefreshScreen();
-    	editorProcessKeypress();
-  }
- 	return 0;
-}
+void editorSetStatusMessage(const char *fmt, ...);
